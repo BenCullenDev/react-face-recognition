32cce43a97b2c84b7de02bd2ef19fcca6f0771c6
Ben Culen <benjaminjcullen1@gmail.com>
2023-01-24 15:02:50 +0000

    added api to clarifai to fetch repsonse for dummy url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
+import React, { Component } from 'react';
 import './App.css';
+import ParticlesBg from 'particles-bg'
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+window.process = {}
 
+const app = new Clarifai.App({
+  apiKey: 'e7139a179fda45d4b25f46a09fdd3ad9'
+});
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super(); 
+      this.state = {
+        input: '',
+      }
+    };
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    app.models
+      .predict(
+        {
+          id: 'face-detection',
+          name: 'face-detection',
+          version: '6dc7e46bc9124c5c8824be4822abe105',
+          type: 'visual-detector',
+        }, 'https://images.newscientist.com/wp-content/uploads/2022/02/14174128/PRI_223554170.jpg'
+        ).then(
+          function(response){
+            console.log(response)
+          },
+          function(err){
+            console.log(err)
+          }
+        )
+console.log('clicked');
+  }
+
+  render() {
+    return(
     <div className="App">
-      <Navigation />
-   
-        <Logo />
-    
       
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange}/>
+      <ParticlesBg type="lines" bg={true}/>
+      <FaceRecognition />
     </div>
-  );
+  );}
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ( {onInputChange, onButtonSubmit} ) => {
     return (
         <div>
-            <p className="f3">
+            <p className="f3 white">
                 {"Want to detect the faces in your image? Upload one now and give it a try!"}
             </p>
             <div className="center">
                 <div className="form pa4 br3 shadow-5 center">
-                    <input className="fa2 pa2 w-70 center"></input>
-                    <button className="w-30 grow f4 link ph3 dib white bg-light-blue">Detect</button></div>
+                    <input className="fa2 pa2 w-70 center" onChange={onInputChange}></input>
+                    <button className="w-30 grow f4 link ph3 dib white bg-light-blue" onClick={onButtonSubmit}>Detect</button></div>
             </div>
         </div>
 
