722460745e4bb4266e33c0c4a9fef48f255206f9
Ben Culen <benjaminjcullen1@gmail.com>
2023-01-25 11:26:58 +0000

    added routing and isSignedIn state to sign in and out links

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
       input: '',
       imgURL: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   };
 
@@ -65,6 +66,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({ route: route});
   }
 
@@ -72,7 +78,7 @@ class App extends Component {
     return (
       <div className="App">
 
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         {this.state.route === 'home' ? 
         <><Logo /><Rank /><ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange} /><ParticlesBg type="lines" bg={true} /><FaceRecognition box={this.state.box} imgURL={this.state.imgURL} /></>
           
